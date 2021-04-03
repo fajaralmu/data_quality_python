5f340a2a731fbff5afdf95e75b971f07ecfed92e
Fajar AM <somabangsa@gmail.com>
2021-04-03 22:19:42 +0700

    Add data_profiling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pandas_profiling
 
 file_name = "retail_raw_reduced_data_quality.csv"
 retail_raw = pd.read_csv(file_name)
@@ -13,3 +14,85 @@ print('Standard Deviation value: ', retail_raw['quantity'].std())
 # Tugas praktek: Deskriptif statistics kolom item_price
 print('')
 print('Kolom item_price')
+# Tugas praktek: Deskriptif statistics kolom item_price
+print('')
+print('Kolom item_price')
+print('Minimum value: ', retail_raw['item_price'].min())
+print('Maximum value: ', retail_raw['item_price'].max())
+print('Mean value: ', retail_raw['item_price'].mean())
+print('Median value: ', retail_raw['item_price'].median())
+print('Standard Deviation value: ', retail_raw['item_price'].std())
+
+# Quantile statistics kolom quantity
+print('Quantile Kolom quantity:')
+print(retail_raw['quantity'].quantile([0.25, 0.5, 0.75]))
+
+# Tugas praktek: Quantile statistics kolom item_price
+print('')
+print('Kolom item_price:')
+print(retail_raw['item_price'].quantile([0.25, 0.5, 0.75]))
+
+print('Korelasi quantity dengan item_price')
+print(retail_raw[['quantity', 'item_price']].corr())
+
+#pandas_profiling.ProfileReport(retail_raw)
+
+# Check kolom yang memiliki missing data
+print('Check kolom yang memiliki missing data:')
+print(retail_raw.isnull().any())
+
+# Filling the missing value (imputasi)
+print('\nFilling the missing value (imputasi):')
+print(retail_raw['quantity'].fillna(retail_raw.quantity.mean()))
+
+# Drop missing value
+print('\nDrop missing value:')
+print(retail_raw['quantity'].dropna())
+
+print(retail_raw['item_price'].fillna(retail_raw['item_price'].mean()))
+
+print("--REMOVE OUTLIER quantity--")
+
+# Q1, Q3, dan IQR
+Q1 = retail_raw['quantity'].quantile(0.25)
+Q3 = retail_raw['quantity'].quantile(0.75)
+IQR = Q3 - Q1
+print("IQR: ", IQR, " q1: ", Q1, " q3: ", Q3)
+
+# Check ukuran (baris dan kolom) sebelum data yang outliers dibuang
+print('Shape awal: ', retail_raw.shape)
+
+# Removing outliers
+retail_raw = retail_raw[~((retail_raw['quantity'] < (Q1 - 1.5 * IQR)) | (retail_raw['quantity'] > (Q3 + 1.5 * IQR)))]
+
+# Check ukuran (baris dan kolom) setelah data yang outliers dibuang
+print('Shape akhir: ', retail_raw.shape)
+
+# ITEM PRICE
+print("--REMOVE OUTLIER item price--")
+
+# Q1, Q3, dan IQR
+Q1 = retail_raw['item_price'].quantile(0.25)
+Q3 = retail_raw['item_price'].quantile(0.75)
+IQR = Q3 - Q1
+
+# Check ukuran (baris dan kolom) sebelum data yang outliers dibuang
+print('Shape awal: ', retail_raw.shape)
+
+# Removing outliers
+retail_raw = retail_raw[~((retail_raw['item_price'] < (Q1 - 1.5 * IQR)) | (retail_raw['item_price'] > (Q3 + 1.5 * IQR)))]
+
+# Check ukuran (baris dan kolom) setelah data yang outliers dibuang
+print('Shape akhir: ', retail_raw.shape)
+
+print("--REMOVE DUPLICATE--")
+
+# Check ukuran (baris dan kolom) sebelum data duplikasi dibuang
+print('Shape awal: ', retail_raw.shape)
+
+# Buang data yang terduplikasi
+retail_raw.drop_duplicates(inplace=True)
+
+# Check ukuran (baris dan kolom) setelah data duplikasi dibuang
+print('Shape akhir: ', retail_raw.shape)
+ 
