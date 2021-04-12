a68da3c30f00996ae1cf063c9af85e16ade143d2
Fajar AM <somabangsa@gmail.com>
2021-04-12 16:08:27 +0700

    add pandas append, concat

---
+++ pandas_manipulation_2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+# Buat series of int (s1) dan series of string (s2)
+s1 = pd.Series([1,2,3,4,5,6])
+s2 = pd.Series(["a","b","c","d","e","f"])
+# Terapkan method append
+s2_append_s1 = s1.append(s2)
+print("Series - append:\n", s2_append_s1)
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({'a':[1,2],
+		   'b':[3,4]})
+df2 = pd.DataFrame({'b':[1,2],
+		   'a':[3,4]})
+# Terapkan method append
+df2_append_df1 = df1.append(df2)
+print("Dataframe - append:\n", df2_append_df1)
+
+#///////////////////////////////////////////
+
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({'a':[1,2],
+					'b':[3,4]})
+df2 = pd.DataFrame({'b':[1,2],
+					'a':[3,4]})
+# Terapkan method concat row-wise
+row_wise_concat = pd.concat([df1,df2])
+print("Row-wise - concat:\n", row_wise_concat)
+# Terapkan method concat column-wise
+col_wise_concat = pd.concat([df1,df2],axis=1)
+print("Column-wise - concat:\n", col_wise_concat)
+# Penambahan identifier --> membentuk hasil penggabungan multiindex
+multiindex_concat = pd.concat([df1,df2],axis=0,keys=['df1','df2'])
+print("Multiindex - concat:\n", multiindex_concat)
